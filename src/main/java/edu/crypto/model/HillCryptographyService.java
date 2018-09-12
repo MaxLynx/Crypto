6150e0f60b4ff8acaf076beebcf423ce6fcc6702
Max <zhirmax@mail.ru>
2018-09-12 20:10:26 +0300

    FilenameConfiguration and ConcreteFileOperatingService added to model

--- src/main/java/edu/crypto/model/HillCryptographyService.java
+++ src/main/java/edu/crypto/model/HillCryptographyService.java
@@ -1,17 +1,18 @@
 package edu.crypto.model;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.io.*;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
-
-import static java.nio.file.StandardOpenOption.APPEND;
-import static java.nio.file.StandardOpenOption.CREATE;
 
 @Component
-public class HillCryptographyService implements CryptographyService, FileOperatingService {
+public class HillCryptographyService implements CryptographyService {
+
+    @Autowired
+    FileOperatingService fileOperatingService;
+
+    @Autowired
+    FilenameConfiguration filenameConfiguration;
 
     public Path encrypt(Path P){
         return P;
@@ -21,45 +22,4 @@ public class HillCryptographyService implements CryptographyService, FileOperati
         return C;
     }
 
-    public String getPathAsString(Path path) {
-
-        byte data[] = new byte[100];
-
-        path = Paths.get(TEMPORARY_FILE_NAME);
-
-        try (InputStream in = new BufferedInputStream(
-                Files.newInputStream(path))) {
-            in.read(data, 0, data.length);
-        } catch (IOException x) {
-            System.err.println(x);
-        }
-
-        destroyFile(path);
-
-        return new String(data);
-    }
-
-    public Path getStringAsPath(String string) {
-
-        Path path = Paths.get(TEMPORARY_FILE_NAME);
-
-        byte data[] = string.getBytes();
-
-        try (OutputStream out = new BufferedOutputStream(
-                Files.newOutputStream(path, CREATE, APPEND))) {
-            out.write(data, 0, data.length);
-        } catch (IOException x) {
-            System.err.println(x);
-        }
-
-        return path;
-    }
-
-    private void destroyFile(Path path){
-        try {
-            Files.deleteIfExists(path);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 }
